aec1000f8c9975a7f079c4a4900a65fe377ac957
guileihu <825848801@qq.com>
2021-12-27 19:36:24 +0800

    add 712

---
+++ 712/acci_common/acci_common/LCS_PRO.h
@@ -0,0 +1,14 @@
+#ifndef _H_LCS_PRO_H_
+#define _H_LCS_PRO_H_
+
+#include <string>
+
+class LCS {
+public:
+	LCS();
+	~LCS();
+	int calculate_lcs(std::string str1, std::string str2);
+};
+
+
+#endif
